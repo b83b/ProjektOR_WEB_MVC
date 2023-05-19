d5f5d0bc24ed07df466fc78e143295f98dca5d0b
Lukasz S <bumer83@wp.pl>
2023-05-19 14:35:45 +0200

    bzrd 17 - dodano klka mozliwosc logowania

--- ProjektORWeb/Constans.cs
+++ ProjektORWeb/Constans.cs
@@ -1,20 +1,47 @@
-﻿namespace ProjektORWeb
+﻿using ProjektORWeb.Models;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace ProjektORWeb
 {
     public class Constans
     {
         public static string ConnectionString = "";
 
+
+        //SQL LOGIN-----------------DOM
+        //public static string ConnectionStr(string login, string haslo)
+        //{
+        //    ConnectionString = $"Data Source=DESKTOP-BBU712F; Initial Catalog=ProjektOR;User ID={login};Password={haslo}; " +
+        //    "Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;" +
+        //    " Integrated Security=False; TrustServerCertificate=True";
+
+        //    return ConnectionString;
+        //}
+        //--------------------------------------------------------------------------------
+
+
+        //WIN LOGIN ---DOM
+        //public static string ConnectionStr(string login, string haslo)
+        //{
+        //    ConnectionString = $"Data Source = DESKTOP-BBU712F; Initial Catalog=ProjektOR; Integrated Security = True; Connect Timeout = 30; " +
+        //        $"Encrypt=False;Trust Server Certificate=True;Application Intent = ReadWrite; Multi Subnet Failover=False";
+
+        //    return ConnectionString;
+        //}
+
+        //Data Source = DESKTOP - BBU712F; Initial Catalog = ProjektOR; Integrated Security = True; Connect Timeout = 30; Encrypt=False;Trust Server Certificate=True;Application Intent = ReadWrite; Multi Subnet Failover=False
+
+        //WIN LOGIN - pd3935------------------------------------------------------------
         public static string ConnectionStr(string login, string haslo)
         {
-            ConnectionString = $"Data Source=DESKTOP-BBU712F; Initial Catalog=ProjektOR;User ID={login};Password={haslo}; " +
-            "Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;" +
-            " Integrated Security=False; TrustServerCertificate=True";
+            ConnectionString = $"Data Source = db-mssql; Initial Catalog=pd3935; Integrated Security = True; Connect Timeout = 30; " +
+                $"Encrypt=False;Trust Server Certificate=True;Application Intent = ReadWrite; Multi Subnet Failover=False";
 
             return ConnectionString;
         }
-        
-
-
+        //--------------------------------------------------------------------
         //SqlConnection con = new SqlConnection(ConnectionString);
         //SqlCommand com = new SqlCommand();
         //com.Connection = con;
--- ProjektORWeb/Models/ProjektOR.cs
+++ ProjektORWeb/Models/ProjektOR.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjektORWeb.Models
 {
+    
     [Table("ProjektOR")]
     public class ProjektOR
     {
